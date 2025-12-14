78a570c09b1c523853ba2afe20db9a38b481ae23
thzguns <matsieguns@gmail.com>
2025-12-14 14:21:29 +0100

    de site pullen van mijn bucket

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,20 +1,3 @@
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow HTTP"
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_instance" "web_app" {
   ami           = "ami-0c02fb55956c7d316" # Ubuntu 22.04
   instance_type = "t2.micro"
@@ -22,13 +5,20 @@ resource "aws_instance" "web_app" {
   security_groups = [aws_security_group.web_sg.name]
 
   user_data = <<-EOF
-              #!/bin/bash
-              apt update
-              apt install -y python3 python3-pip
-              pip3 install flask boto3
-              mkdir -p /home/ubuntu/app
-	      git clone https://github.com/ThzGuns/CloudForSSS/tree/master/web /home/ubuntu/app
-              EOF
+    #!/bin/bash
+    apt update
+    apt install -y python3 python3-pip awscli
+    pip3 install flask boto3
+
+    mkdir -p /home/ubuntu/app
+    cd /home/ubuntu/app
+
+    # Download alle website bestanden van S3
+    aws s3 cp s3://${BucketName}/ /home/ubuntu/app/ --recursive
+
+    # Start Flask app op achtergrond
+    nohup python3 /home/ubuntu/app/app.py &
+  EOF
 
   tags = { Name = "web-app-instance" }
 }
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,10 @@
 resource "aws_s3_bucket" "uploads" {
   bucket = var.BucketName
 }
+
+resource "aws_s3_bucket_object" "web_files" {
+  for_each = fileset("../web", "**")  # alles in ../web
+  bucket   = aws_s3_bucket.uploads.id
+  key      = each.value
+  source   = "../web/${each.value}"
+}
