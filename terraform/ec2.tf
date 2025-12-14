4f09daa14c8cb19530e3c5c400973f333bfd6a73
thzguns <matsieguns@gmail.com>
2025-12-14 14:25:07 +0100

    verbeterd ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -5,20 +5,39 @@ resource "aws_instance" "web_app" {
   security_groups = [aws_security_group.web_sg.name]
 
   user_data = <<-EOF
-    #!/bin/bash
-    apt update
-    apt install -y python3 python3-pip awscli
-    pip3 install flask boto3
+  #!/bin/bash
+  apt update
+  apt install -y python3 python3-pip awscli
+  pip3 install flask boto3
 
-    mkdir -p /home/ubuntu/app
-    cd /home/ubuntu/app
+  mkdir -p /home/ubuntu/app
+  cd /home/ubuntu/app
 
-    # Download alle website bestanden van S3
-    aws s3 cp s3://${BucketName}/ /home/ubuntu/app/ --recursive
+  aws s3 cp s3://${var.BucketName}/ /home/ubuntu/app/ --recursive
 
-    # Start Flask app op achtergrond
-    nohup python3 /home/ubuntu/app/app.py &
+  nohup python3 /home/ubuntu/app/app.py &
   EOF
+
 
   tags = { Name = "web-app-instance" }
 }
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP"
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "uploads" {
   bucket = var.BucketName
 }
 
-resource "aws_s3_bucket_object" "web_files" {
+resource "aws_s3_object" "web_files" {
   for_each = fileset("../web", "**")  # alles in ../web
   bucket   = aws_s3_bucket.uploads.id
   key      = each.value
