cca73c3b10e933dd0894a775ac3e7c7d2e96b9be
thzguns <matsieguns@gmail.com>
2025-12-15 23:18:38 +0100

    wat site aanpassingen troubleshooting

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
 sudo apt update -y
+sudo apt install -y apache2 php php-mysql mysql-server unzip curl
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+sudo systemctl start apache2
+sudo systemctl enable apache2
+sudo systemctl start mysql
+sudo systemctl enable mysql
+
+sudo rm -rf /var/www/html/*
+sudo aws s3 cp s3://terra-bucket-cloud/ /var/www/html/ --recursive
+
+sudo chown -R www-data:www-data /var/www/html/
+sudo chmod -R 755 /var/www/html/
+
+#!/bin/bash
+
+sudo apt update -y
 sudo apt install -y apache2 php php-mysql mysql-server unzip
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -20,7 +40,8 @@ sudo chmod -R 755 /var/www/html/
 
 sudo mysql -u root <<EOF
 CREATE DATABASE files_db;
-CREATE USER 'appuser'@'localhost' IDENTIFIED BY 'password123';
+DROP USER IF EXISTS 'admin'@'localhost';
+CREATE USER 'admin'@'localhost' IDENTIFIED WITH mysql_native_password BY 'SuperSecure123!';
 GRANT ALL PRIVILEGES ON files_db.* TO 'appuser'@'localhost';
 FLUSH PRIVILEGES;
 
@@ -33,5 +54,7 @@ CREATE TABLE files (
 );
 EOF
 
+sudo systemctl restart apache2
+sudo systemctl restart mysql
 sudo systemctl restart apache2
 sudo systemctl restart mysql
