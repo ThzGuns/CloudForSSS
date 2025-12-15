f758b9c7952d0bc34514c98c8ad8568fe02cc72d
thzguns <matsieguns@gmail.com>
2025-12-15 11:58:02 +0100

    ec2 update en rules

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,11 +1,14 @@
 resource "aws_instance" "web_app" {
-  ami           = "ami-09c54d172e7aa3d9a"
-  instance_type = "t3.micro"
-  key_name      = "KeyMaterial"
-
+  ami             = "ami-09c54d172e7aa3d9a"
+  instance_type   = "t3.micro"
+  key_name        = "KeyMaterial"
   security_groups = [aws_security_group.web_sg.name]
 
-  user_data = file("user_data.sh") # script to install web server + APP files
+  # ADD THIS LINE
+  iam_instance_profile = aws_iam_instance_profile.ec2_s3_profile.name
+
+  user_data               = file("user_data.sh")
+  user_data_replace_on_change = true  # Recommended
 
   tags = {
     Name = "AppServer"
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -45,3 +45,47 @@ resource "aws_iam_role_policy" "lambda_s3_dynamodb" {
     ]
   })
 }
+
+resource "aws_iam_role" "ec2_s3_role" {
+  name = "ec2-s3-read-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "ec2_s3_read_policy" {
+  name = "ec2-s3-read-policy"
+  role = aws_iam_role.ec2_s3_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.uploads.arn,
+          "${aws_s3_bucket.uploads.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_s3_profile" {
+  name = "ec2-s3-profile"
+  role = aws_iam_role.ec2_s3_role.name
+}
