32cfba8cc3297c115809bf6777ede2098b9cb38b
thzguns <matsieguns@gmail.com>
2025-12-16 16:59:11 +0100

    update main.tf en var 3

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,11 +14,3 @@ variable "ami" {
   type        = string
   default     = "ami-049442a6cf8319180" 
 }
-
-variable "AWSAccessKey" {
-  default = ""  # leave empty in code
-}
-
-variable "AWSSecretKey" {
-  default = ""
-}
